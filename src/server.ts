adfe941529a1ca333000c694d3a4e8a2b65f4d19
SanjarSanchez <sanjar@ajou.ac.kr>
2026-05-24 13:41:30 +0900

    feat:integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,2 @@
-// Architechtural pattern: MVC, Dependency Injection (NESTJS), MVP
-
-// MVC = MODEL VIEW CONTROLLER Asosan shu ishlatiladi
-
-// Design pattern: Middleware, Decotar 
+import dotenv from 'dotenv';
+dotenv.config();
--- train.ts
+++ train.ts
@@ -1,8 +1,21 @@
 //  MIT TASK - L
 
-function reverseSentence(str: string): string {
-  return str.split(' ').map((word: string) => word.split('').reverse().join('')).join(' ');
+// function reverseSentence(str: string): string {
+//   return str.split(' ').map((word: string) => word.split('').reverse().join('')).join(' ');
+// }
+
+// // Test
+// console.log(reverseSentence("we like coding!"));  // "ew ekil !gnidoc"
+
+//  MIT TASK - M
+interface SquareNumber {
+  number: number;
+  square: number;
+}
+
+function getSquareNumbers(arr: number[]): SquareNumber[] {
+  return arr.map((num: number): SquareNumber => ({ number: num, square: num * num }));
 }
 
 // Test
-console.log(reverseSentence("we like coding!"));  // "ew ekil !gnidoc"
+console.log(getSquareNumbers([1, 2, 3]));
