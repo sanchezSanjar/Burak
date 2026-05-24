56d8788c4c90b86d98c5e6d32b6d5d98a78800fc
SanjarSanchez <sanjar@ajou.ac.kr>
2026-05-24 15:13:22 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from "path";
-
+import router from "./router";
 
 /** 1-ENTRANCE **/
 const app = express();
@@ -15,6 +15,8 @@ app.use(express.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); 
 
-/** 4-ROUTERS **/
+/** 4-ROUTERS **/ 
+app.use("/", router); // Middleware Design Pattern
+
 
 export default app;  //
