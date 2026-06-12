8dd10ed2933ef06e94b3211cb34b09a149d59288
SanjarSanchez <sanjar@ajou.ac.kr>
2026-06-12 17:18:05 +0900

    fix:modify UpdateChosenProduct business logic

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,5 +1,6 @@
+import { shapeIntoMongooseObjectId } from "../libs/config";
 import Errors, { HttpCode, Message } from "../libs/Errors";
-import { Product, ProductInput } from "../libs/types/product";
+import { Product, ProductInput, ProductUpdateInput } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 
 class ProductService {
@@ -23,6 +24,18 @@ public async createNewProduct (input: ProductInput): Promise<Product>{
 
     }
  }
+public async updateChosenProduct(
+    id: string,
+    input: ProductUpdateInput,
+  ): Promise<Product> {
+    // string => ObjectId
+    id = shapeIntoMongooseObjectId(id);
+    const result = await this.productModel
+      .findOneAndUpdate({ _id: id }, input, { new: true })
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
 
- }
+    return result;
+  }
+}
 export default ProductService;
