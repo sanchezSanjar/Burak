0ede2a3cd34d203d7c8a752f5f9c048ae293e357
SanjarSanchez <sanjar@ajou.ac.kr>
2026-06-12 16:01:42 +0900

    feat: createNewProduct business logic by restaurant member

--- src/schema/Product.model.ts
+++ src/schema/Product.model.ts
@@ -42,19 +42,18 @@ const productSchema = new Schema(
         },
 
         productVolume: {
-            type: String,
+            type: Number,
             enum: ProductVolume,
             default: ProductVolume.ONE,
         },
 
         productDesc: {
             type: String,
-            required: true,
         },
 
         productImages: {
             type: [String],
-            default: []
+            default: [],
         },
 
         productViews: {
