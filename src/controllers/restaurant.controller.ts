47e092aa6e8d1c4338e90974dc772ef5d944ca7f
SanjarSanchez <sanjar@ajou.ac.kr>
2026-06-17 17:41:07 +0900

    feat: develop updateChosenUser api

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -113,7 +113,7 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
     try { 
         console.log('getUsers');
         const result = await memberService.getUsers();
-        console.log("result:", result);
+        console.log("result:", result); 
 
         res.render("users", {users: result});
     } catch (err) {
@@ -121,11 +121,16 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
         res.redirect("/admin/login");
     } 
 };
-restaurantController.updateChosenUser = (req: Request, res: Response) => {
+restaurantController.updateChosenUser = async (req: Request, res: Response) => {
     try { 
         console.log('updateChosenUser');
+        const result = await memberService.updateChosenUser(req.body);
+
+        res.status(HttpCode.OK).json({data: result});
     } catch (err) {
         console.log("Error, updateChosenUser:", err);
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
         
     } 
 };
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -35,8 +35,12 @@ routerAdmin.post(
     productController.updateChosenProduct);
 
 /** User */
-routerAdmin.get("/user/all", restaurantController.verifyRestaurant, 
-    restaurantController.getUsers)
+routerAdmin.get("/user/all", 
+    restaurantController.verifyRestaurant, 
+    restaurantController.getUsers);
+routerAdmin.post("/user/edit", 
+    restaurantController.verifyRestaurant, 
+    restaurantController.updateChosenUser);
 
 
 export default routerAdmin;
