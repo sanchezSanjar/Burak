c9ae13da61e05b0a849e82e19951937dcf5e6413
SanjarSanchez <sanjar@ajou.ac.kr>
2026-06-24 12:30:32 +0900

    feat: create new product frontend logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -33,6 +33,7 @@ productController.createNewProduct = async (
     req: AdminRequest, res: Response) => {
     try {
         console.log("createNewProduct");
+        console.log("req.body:", req.body);
         if(!req.files?.length) 
             throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
