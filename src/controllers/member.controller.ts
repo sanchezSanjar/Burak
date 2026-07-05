260b3592bb875986d9c42eb66a6d2a227a59869a
SanjarSanchez <sanjar@ajou.ac.kr>
2026-07-06 08:19:51 +0900

    feat:develop verify authenticated user credentials

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response} from "express";
 import {T} from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
-import Errors from "../libs/Errors";
+import Errors, { Message } from "../libs/Errors";
 import AuthService from "../schema/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
 import { HttpCode } from "../libs/Errors";
@@ -17,6 +17,7 @@ memberController.signup = async (req: Request, res: Response) => {
         const input: MemberInput = req.body,
         result: Member = await memberService.signup(input);
         const token = await authService.createToken(result);
+
         res.cookie("accessToken", token, {
             maxAge:AUTH_TIMER *3600 *1000,
             httpOnly:false,
@@ -24,11 +25,10 @@ memberController.signup = async (req: Request, res: Response) => {
 
         res.status(HttpCode.CREATED).json({ member: result, accessToken: token});
         
-        res.json({member: result});
     } catch (err) {
         console.log("Error, signup :", err);
         if(err instanceof Errors) res.status(err.code).json(err);
-        else res.status(Errors.standard.code).json(Errors.standard.message);
+        else res.status(Errors.standard.code).json(Errors.standard);
     }  
 }; 
 
@@ -48,8 +48,26 @@ memberController.login = async (req: Request, res: Response) => {
     } catch (err) {
         console.log("Error, login :", err);
         if(err instanceof Errors) res.status(err.code).json(err);
-        return res.status(Errors.standard.code).json(Errors.standard.message);
+        else res.status(Errors.standard.code).json(Errors.standard);
     } 
+};
+
+memberController.verifyAuth = async (req: Request, res: Response) => {
+   try {
+    let member = null;
+    const token = req.cookies["accessToken"];
+    if(token) member = await authService.checkAuth(token);
+
+    if(!member)
+        throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
+
+    console.log("member:", member);
+    res.status(HttpCode.OK).json({member:member});
+   } catch (err) {
+    console.log("Error, verifyAuth:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+   }
 };
 export default memberController;
 
--- src/schema/Auth.service.ts
+++ src/schema/Auth.service.ts
@@ -5,7 +5,11 @@ import jwt from "jsonwebtoken";
 
 
 class AuthService {
-    constructor() {}
+    private readonly secretToken;
+
+    constructor() {
+        this.secretToken = process.env.SECRET_TOKEN as string;
+    }
 
     public async createToken(payload: Member) {
         return new Promise((resolve, reject) =>{
@@ -15,12 +19,22 @@ class AuthService {
                         expiresIn: duration , 
                     },
                      (err, token)=> {
-                        if(err) reject(new Errors(HttpCode.UNAUTHORIZED, Message.TOKEN_CREATION_FAILED)
+                        if(err) 
+                            reject(new Errors(HttpCode.UNAUTHORIZED, Message.TOKEN_CREATION_FAILED)
                         );
                     else resolve(token as string);
                     }
              );
         });
+    }
+
+    public async checkAuth(token: string): Promise<Member> {
+        const result: Member = (await jwt.verify(
+            token,
+             this.secretToken
+            )) as Member;
+            console.log(`-----[AUTH] memberNick: ${result.memberNick} ----`);
+            return result;
     }
 }
 
